8ef662dab2d8e6a2cdea9c0ccc14800916638318
gutoggg <72248716+gutoggg@users.noreply.github.com>
2022-03-22 07:55:03 -0300

    Divide main.js em arquivos diferentes

---
+++ assets/js/headerMenu.js
@@ -0,0 +1,21 @@
+(() => {
+    const ulNavigation = document.querySelector('.header-navigation')
+    const liNavigationList = document.querySelectorAll('.nav-list-item')
+
+    const menuButton = document.querySelector('.menu-icon')
+    const closeButton = document.querySelector('.close-icon')
+
+    menuButton.addEventListener('click', () => {
+        ulNavigation.classList.toggle('show')
+    })
+
+    closeButton.addEventListener('click', () => {
+        ulNavigation.classList.toggle('show')
+    })
+
+    for (const element of liNavigationList) {
+        element.addEventListener('click', () => {
+            ulNavigation.classList.remove('show')
+        })
+    }
+})()
